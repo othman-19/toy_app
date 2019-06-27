044b4aebc3b4572bbad353bca0e5e224746808cc
othmane <othman-19@hotmail.fr>
2019-06-27 11:06:23 +0100

    add hello world

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 Rails.application.routes.draw do
-  root 'application#toy_app'
+  root 'application#hello'
 end
-
